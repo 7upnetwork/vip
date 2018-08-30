a9ad1b1e6529b15ff1574859fadf7183e1219089
7upnetwork <42762406+7upnetwork@users.noreply.github.com>
2018-08-30 20:46:54 +0700

    Update 7upviptrial12.lua

--- 7upviptrial12.lua
+++ 7upviptrial12.lua
@@ -1,1 +1,1 @@
-print(os.date("Today : %Y/%m/%d Time : %H:%M:%S\n\nTrial period has expired ! \nPlease contact administrator for pay VIP version.")
+print("Trial period has expired ! \nPlease contact administrator for pay VIP version.")
